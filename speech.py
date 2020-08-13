605323995cdf259c8d5697d09af0f67f458bfa5c
Dheeraj <dheerajqm@gmail.com>
2020-08-13 16:02:00 +0530

    first

---
+++ speech.py
@@ -0,0 +1,24 @@
+
+# importing the pyttsx library 
+import pyttsx3 
+  
+# initialisation 
+engine = pyttsx3.init() 
+newVoiceRate = 90
+engine.setProperty('rate',newVoiceRate)
+# testing 
+engine.say("I am a High-functioning Sociopath") 
+
+engine.runAndWait() 
+print("SEE NOTIFICATION")
+
+from plyer import notification
+def notify(title, message):
+     notification.notify(
+          message = message,
+          title = title,
+          app_icon = None,
+          timeout = 3
+     )
+if __name__ == "__main__":
+    notify("Dheeraj Verma", "High-functioning Sociopath")
